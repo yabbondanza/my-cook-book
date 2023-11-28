aac85a4ba27e54ae880b51ed6ccaf74e236e8a95
yabbondanza <yasmimabbondanza@gmail.com>
2023-11-27 22:23:58 -0300

    Feat: Model de receitas e conexão com o banco de dados

---
+++ api/mycookbook/src/main/java/com/cookingclub/mycookbook/model/Receita.java
@@ -0,0 +1,40 @@
+package com.cookingclub.mycookbook.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Receita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "receita_id")
+    private Long idReceita;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "ingredientes")
+    private String ingredientes;
+
+    @Column(name = "preparo")
+    private String preparo;
+
+    @Column(name = "tipo_versao")
+    private String tipoVersao;
+
+    @Column(name = "receita_original")
+    private Long receitaOriginal;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+    @Column(name = "data_hora")
+    private Date dataHora;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
