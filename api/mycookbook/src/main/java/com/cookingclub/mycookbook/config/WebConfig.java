cc6f440e81335d3d49018cc3f3e3a99acb2f6991
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2023-12-05 12:05:03 -0300

    Fix: adicionando HEAD e PATCH ao CORS

--- api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
+++ api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowCredentials(true)
                 .allowedHeaders("*");
     }
