79579066895309fc2d54a61abfd5be04159bec9d
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2023-12-05 10:52:31 -0300

    Fix: adicionando allowedHeaders

--- api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
+++ api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
@@ -8,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMapping(CorsRegistry registry) {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowCredentials(true)
-        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .allowedHeaders("*");
     }
 }
