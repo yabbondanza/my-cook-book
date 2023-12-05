3f036b7ff897ed7e6e05ea74e7ddd16f3205e6f9
Michel Alexandrino de Souza <michelsouza7864@gmail.com>
2023-12-05 16:43:23 -0300

    Fix: correção no WebConfig

--- api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
+++ api/mycookbook/src/main/java/com/cookingclub/mycookbook/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowCredentials(true)
